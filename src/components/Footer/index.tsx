93b9001d709209b0447abdd6652e5786385be41e
Obiora Igboanusi <cwertlinks@gmail.com>
2022-07-24 17:40:23 +0100

    Implement footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div>
+      <small>
+        <p className='text-center'>© 2021 Obiora Igboanusi</p>
+      </small>
+    </div>
+  );
 }
 
 export default Footer
