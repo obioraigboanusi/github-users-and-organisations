147331cb82c5706783cf2945192af0261a0a438e
Obiora Igboanusi <cwertlinks@gmail.com>
2022-07-24 17:36:35 +0100

    fix error

--- src/api/AxiosInstance.ts
+++ src/api/AxiosInstance.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-export default axios.create({
+const AxiosInstance = axios.create({
   baseURL: "https://api.github.com/",
   headers: {
     "Content-Type": "application/json",
   },
 });
+export default AxiosInstance;
